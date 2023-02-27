29b633b36c93326c062b01ad3d074bda54c5b550
Juan Felipe Aguas Pulido <juan.aguas@mail.escuelaing.edu.co>
2023-02-27 14:02:57 -0500

    first commit

---
+++ src/main/java/ada/escuelaing/h2mem/H2memApplication.java
@@ -0,0 +1,13 @@
+package ada.escuelaing.h2mem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2memApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2memApplication.class, args);
+	}
+
+}
